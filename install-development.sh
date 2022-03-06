f654c82066856ffe5bd322f8e940981ed00e85a5
Eduardo Judici <edujudici@gmail.com>
2022-03-05 23:51:51 -0300

    feat: dockerize application

---
+++ install-development.sh
@@ -0,0 +1,69 @@
+echo "==============================================="
+echo "Project - DEPLOYMENT"
+echo "==============================================="
+
+ENV_FILE=.env
+if [ ! -f "$ENV_FILE" ]; then
+    echo "-----------------------------------------------"
+    echo "Project - Copy env file config."
+    echo "-----------------------------------------------"
+    cp .env.example .env
+fi
+
+if ! grep -q "USER=" "$ENV_FILE"; then
+    echo "-----------------------------------------------"
+    echo "Project - Set USER on env file."
+    echo "-----------------------------------------------"
+    echo "USER=${USER}" >> .env
+fi
+
+if ! grep -q "UID=" "$ENV_FILE"; then
+    echo "-----------------------------------------------"
+    echo "Project - Set UID on env file."
+    echo "-----------------------------------------------"
+    echo "UID=${UID}" >> .env
+fi
+
+echo "-----------------------------------------------"
+echo "Project - Building steps and up all containers."
+echo "-----------------------------------------------"
+docker-compose up -d --build
+
+echo "-----------------------------------------------"
+echo "Project - Waiting start all containers..."
+echo "-----------------------------------------------"
+sleep 15
+
+echo "-----------------------------------------------"
+echo "Project - Install/update composer dependecies."
+echo "-----------------------------------------------"
+docker-compose exec app composer install
+
+echo "-----------------------------------------------"
+echo "Project - Generating key."
+echo "-----------------------------------------------"
+docker-compose exec app php artisan key:generate
+
+echo "-----------------------------------------------"
+echo "Project - Artisan is migrating DB."
+echo "-----------------------------------------------"
+docker-compose exec app php artisan migrate
+
+echo "-----------------------------------------------"
+echo "Project - Fill data on DB."
+echo "-----------------------------------------------"
+docker-compose exec app php artisan db:seed
+
+#echo "-----------------------------------------------"
+#echo "Project - Build assets using mode development."
+#echo "-----------------------------------------------"
+# npm run dev
+
+echo "-----------------------------------------------"
+echo "Project - Show all containers."
+echo "-----------------------------------------------"
+docker ps
+
+echo "-----------------------------------------------"
+echo "Project - LIVE."
+echo "-----------------------------------------------"
